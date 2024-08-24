3ee45698002be6d038c5d61e5b6ada245f66c06e
mahongqin <codepapercut@gmail.com>
2024-08-24 23:41:44 +0800

    Fix dark

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { useTheme } from 'next-themes';
+
 export default function IndexPage() {
+  const { setTheme } = useTheme();
   return (
     <div>
       <div className="overflow-hidden rounded-lg bg-background">
+        <Button onClick={() => setTheme("light")}>light</Button>
+        <Button onClick={() => setTheme("dark")}>dark</Button>
         Helloword
       </div>
     </div>
